39eff809ca1d5a5c6a51945f424e2302f3464c2d
405787704@qq.com <405787704@qq.com>
2024-10-25 14:09:44 +0800

    [feat:]合并分支前保存修改

--- Assets/Scripts/Twentytoo/UI/SLPanel.cs
+++ Assets/Scripts/Twentytoo/UI/SLPanel.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SLPanel : BasePanel
 {
+    /*选中Save->选中一个存档位->确定->如果存档位为空 弹出提示框 存档成功
+                                    ->不为空 弹出是否覆盖存档
+      选中Load->选中一个存档位->确定->读档*/
     public override void HideMe()
     {
         gameObject.SetActive(false);
